225b56be9fda4cdf1ad4f7eef85a3fcca0673d7b
HoonC-corgi <tgh7544@naver.com>
2022-11-17 15:55:06 +0900

    NumberFormatException handling apply "try, catch"

--- src/main/java/NumberFormatException/Main.java
+++ src/main/java/NumberFormatException/Main.java
@@ -1,4 +1,19 @@
 package NumberFormatException;
 
 public class Main {
+    public static void main(String[] args) {
+        String stringNumber [] = {"23", "12", "3.141592", "998"};
+
+        int i=0;
+        for(i=0; i<stringNumber.length; i++) {
+            try{
+                int j = Integer.parseInt(stringNumber[i]);  // 배열을 정수로 형변환
+                // 3.141592는 정수 형태로 변환 불가 >> 오류발생
+                System.out.println("숫자로 변환된 값은 " + j);
+            }
+            catch(NumberFormatException e) {
+                System.out.println(stringNumber[i] + "는 정수로 변환할 수 없습니다.");
+            }
+        }
+    }
 }
